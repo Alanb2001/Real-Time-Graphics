d0903fba98233c6b8021d26961ae95ff13e47c93
Alan Bednarski <78023436+Alanb2001@users.noreply.github.com>
2022-11-03 12:57:25 +0000

    Multi pass nearly works now

--- ThreeGPStart/Data/Shaders/lights_FS.glsl
+++ ThreeGPStart/Data/Shaders/lights_FS.glsl
@@ -1,42 +1,44 @@
 #version 330
 
-layout(std140) uniform PerFrameUniforms
-{
-	vec3 light_position;
-	float light_range;
-	vec3 light_direction;
-	vec3 cam_pos;
-	vec3 ambeint_light;
-};
+uniform sampler2D sampler_tex;
+uniform vec3 camera_position;
+
+uniform vec3 light_colour;
+uniform vec3 light_position;
+
+in vec3 varying_position; 
+in vec3 varying_normal;
+in vec2 varying_coord;
+in float varying_gouraud;
 
 out vec4 fragment_colour;
 
-in vec3 colour;
-in vec3 normal;
-in vec3 position; 
-
 void main(void)
-{	
-	vec3 Ka = colour;
-	vec3 Kd = colour;
-	vec3 Ks = vec3(1, 1, 1);
-	vec3 Ia = ambeint_light;
+{
+    float light_range = 3000;
+    float material_shininess = 20;
 
-	vec3 mycolour = (Ia * Ka);
+    vec3 material_diffuse_colour = texture(sampler_tex, varying_coord).rgb;
+    vec3 fragment_position = varying_position;
+    vec3 fragment_normal = normalize(varying_normal);
 
-	vec3 P = position;
-	vec3 N = normalize(normal);
-	vec3 V = (cam_pos - P);
+    vec3 material_ambient_colour = material_diffuse_colour;
+    vec3 material_specular_colour = material_diffuse_colour;
 
-	float atten = 1 - smoothstep(0, light_range, distance(P, light_position));
-	vec3 Il = vec3(0.5, 0.5, 0.5) * atten;
-	vec3 L = normalize(light_position - P);
-	vec3 R = reflect(-L, N);
-	float specular = 0;
+    vec3 light_direction = normalize(light_position - fragment_position);
+    vec3 camera_direction = normalize(camera_position - fragment_position);
 
-	vec3 lambert_col = Il * (Kd * max (dot(N, L), 0.0) + Ks * specular);
+    float light_distance = distance(light_position, fragment_position);
+    float attenuation = 1.0 - smoothstep(0, light_range, light_distance);
 
-	mycolour += lambert_col;
+    float light_amount = max(0, dot(light_direction, fragment_normal));
+    vec3 diffuse_colour = material_diffuse_colour * light_amount;
 
-	fragment_colour = vec4(mycolour, 1.0);
+    vec3 rV = reflect(-light_direction, fragment_normal);
+    float LR = max(0, dot(camera_direction, rV));
+    vec3 specular_colour = material_specular_colour * pow(LR, material_shininess);
+
+    vec3 final_colour = light_colour * (diffuse_colour + specular_colour) * attenuation;
+
+    fragment_colour = vec4(light_amount * final_colour, 1.0);
 }
--- ThreeGPStart/Data/Shaders/lights_VS.glsl
+++ ThreeGPStart/Data/Shaders/lights_VS.glsl
@@ -1,27 +1,21 @@
 #version 330
 
-layout(std140) uniform PerModelUniforms
-{
-	mat4 projection_xform;
-	mat4 model_xform;
-	mat4 view_xform;
-	vec3 material_colour;
-	float pad1_;
-};
+uniform mat4 combined_xform;
+uniform mat4 model_xform;
 
-in vec3 vertex_position;
-in vec3 vertex_normal;
+layout(location = 0) in vec3 vertex_position;
+layout(location = 1) in vec3 vertex_normal;
+layout(location = 2) in vec2 vertex_texturecoord;
 
-out vec3 colour;
-out vec3 normal;
-out vec3 position;
+out vec3 varying_position; 
+out vec3 varying_normal;
+out vec2 varying_coord;
 
 void main(void)
 {
-	colour = material_colour;
-	normal = vec3(model_xform * vec4(vertex_normal, 0));
-	position = vec3(model_xform * vec4(vertex_position, 1.0));
+	varying_position = mat4x3(model_xform) * vec4(vertex_position, 1.0);
+	varying_normal = mat3(model_xform) * vertex_normal;
+	varying_coord = vertex_texturecoord;
 
-	mat4 combined_xform = projection_xform * view_xform * model_xform;
-	gl_Position = combined_xform * vec4(vertex_position, 1.0);
+	gl_Position = combined_xform * model_xform * vec4(vertex_position, 1.0);
 }
